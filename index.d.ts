37b92dbe76d7fe86b389aa22c605348b0bf221ef
Willy Wainaina Ng'ang'a <67837570+willyngash@users.noreply.github.com>
2023-09-23 13:22:14 +0300

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-native-screens-response' {
+
+  export function widthPercentageToDP(widthPercent: string | number): number;
+  export function heightPercentageToDP(heightPercent: string | number): number;
+}
