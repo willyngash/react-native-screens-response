869dbff551a501a73c9ce12af08247b9e07fef93
Willy Wainaina Ng'ang'a <67837570+willyngash@users.noreply.github.com>
2023-11-02 15:07:09 +0300

    Delete index.d.ts
    delete file

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-native-screens-response' {
-
-  export function widthPercentageToDP(widthPercent: string | number): number;
-  export function heightPercentageToDP(heightPercent: string | number): number;
-}
